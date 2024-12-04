257338ecfcaa45b793d1c8253dbed58c08d5ff79
JohanCodeForFun <hellberg.johan@hotmail.com>
2024-12-04 20:51:32 +0100

    add hero products component

--- src/App.vue
+++ src/App.vue
@@ -2,23 +2,9 @@
 import { RouterView } from "vue-router";
 
 import TheHeader from "./components/TheHeader.vue";
+import HeroProducts from "./components/HeroProducts.vue";
 
 // fetch('https://fakestoreapi.com/products')
-
-interface Rating {
-  rate: number;
-  count: number;
-}
-
-interface Product {
-  id: number;
-  title: string;
-  price: number;
-  description: string;
-  category: string;
-  image: string;
-  rating: Rating;
-}
 </script>
 
 <template>
@@ -29,41 +15,9 @@ interface Product {
   <main>
     <p>home{{ $route.fullPath }}</p>
     <h1>{{ message }}</h1>
-    <div class="hero-products" v-if="data && data.length > 15">
-      <div class="product-card">
-        <img :src="data[15].image" width="300" height="auto" alt="" />
-        <p>{{ data[15].title }}</p>
-        <p>{{ data[15].description }}</p>
-        <p>
-          <span>
-            Price: {{ data[5].price }}
-            <button>Add to cart</button>
-          </span>
-        </p>
-      </div>
-      <div class="product-card">
-        <img :src="data[2].image" width="300" height="auto" alt="" />
-        <p>{{ data[2].title }}</p>
-        <p>{{ data[2].description }}</p>
-        <p>
-          <span>
-            Price: {{ data[5].price }}
-            <button>Add to cart</button>
-          </span>
-        </p>
-      </div>
-      <div class="product-card">
-        <img :src="data[5].image" width="300" height="auto" alt="" />
-        <p>{{ data[5].title }}</p>
-        <p>{{ data[5].description }}</p>
-        <p>
-          <span>
-            Price: {{ data[5].price }}
-            <button>Add to cart</button>
-          </span>
-        </p>
-      </div>
-    </div>
+
+    <HeroProducts :products="data"/>
+    
     <RouterView />
   </main>
 </template>
---
+++ src/components/HeroProducts.vue
@@ -0,0 +1,46 @@
+<script setup lang="ts">
+import { defineProps } from 'vue';
+import type { Product } from '@/types/interface';
+
+const props = defineProps<{
+  products: Product[] | null;
+}>();
+</script>
+
+<template>
+  <div class="hero-products" v-if="props.products && props.products.length > 15">
+      <div class="product-card">
+        <img :src="props.products[15].image" width="300" height="auto" alt="" />
+        <p>{{ props.products[15].title }}</p>
+        <p>{{ props.products[15].description }}</p>
+        <p>
+          <span>
+            Price: {{ props.products[5].price }}
+            <button>Add to cart</button>
+          </span>
+        </p>
+      </div>
+      <div class="product-card">
+        <img :src="props.products[2].image" width="300" height="auto" alt="" />
+        <p>{{ props.products[2].title }}</p>
+        <p>{{ props.products[2].description }}</p>
+        <p>
+          <span>
+            Price: {{ props.products[5].price }}
+            <button>Add to cart</button>
+          </span>
+        </p>
+      </div>
+      <div class="product-card">
+        <img :src="props.products[5].image" width="300" height="auto" alt="" />
+        <p>{{ props.products[5].title }}</p>
+        <p>{{ props.products[5].description }}</p>
+        <p>
+          <span>
+            Price: {{ props.products[5].price }}
+            <button>Add to cart</button>
+          </span>
+        </p>
+      </div>
+    </div>
+</template>
